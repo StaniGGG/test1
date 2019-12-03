05cc58bb0a898c485f700b1e86c86773f47c3e9c
Stanislav Gabco <eineaugekatze@gmail.com>
2019-12-03 19:25:59 +0100

    Zadefinovany list + main

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct list {                               //ZADEFINUJEME NAS LINKED LIST
+    char kategoria[50];
+    char miesto[50];
+    char ulica[100];
+    int rozloha;
+    int cena;
+    char popis[200];
+    struct list *next;
+};
 
+typedef struct list LIST;                  //OZNACIME PREMENNU LIST
 
 
 
 int main(void){
-
+LIST *head=NULL;
+    char x;
+    scanf("%c", &x);
 }
